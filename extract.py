169219b26531ddf30b3d0291c512c4dec6d36d1f
YoshimatsuSaito <middle158725071@gmail.com>
2024-11-17 09:30:15 +0000

    [change] config structure

--- extract.py
+++ extract.py
@@ -21,34 +21,46 @@ BUCKET_NAME = os.environ["BUCKET_NAME"]
 
 
 def main() -> None:
-    # 取得設定の読み込み
+    # 設定の読み込み
     config_path = Path("./config/config.yml")
     dict_config = load_config(config_path)
+    start_year = dict_config["start_year"]
+    end_year = dict_config["end_year"]
+    list_market = dict_config["markets"]
+    list_genres = dict_config["genres"]
+    num_to_search = dict_config["num_to_search"]
 
     # データの取得
     sh = SpotipyHandler(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
-    df_result_search = sh.search(**dict_config)
-    df_audio_features = sh.get_audio_features(df_result_search["track_id"].tolist())
-    df = pd.merge(
-        df_audio_features,
-        df_result_search.loc[:, ["name", "popularity", "track_id"]],
-        how="left",
-        left_on=["id"],
-        right_on=["track_id"],
-    ).reset_index(drop=True)
+    for market in list_market:
+        for year in range(start_year, end_year + 1):
+            for genre in list_genres:
+                print(f"Getting data of {market}-{year}-{genre} from api")
+                df_result_search = sh.search(
+                    year=year, market=market, genre=genre, num_to_search=num_to_search
+                )
+                df_audio_features = sh.get_audio_features(
+                    df_result_search["track_id"].tolist()
+                )
+                df = pd.merge(
+                    df_audio_features,
+                    df_result_search.loc[:, ["name", "popularity", "track_id"]],
+                    how="left",
+                    left_on=["id"],
+                    right_on=["track_id"],
+                ).reset_index(drop=True)
 
-    # タイトルの感情分析結果の追加
-    sa = SentimentAnalyzer()
-    df_sa = sa.classify(df["name"].tolist())
-    df = pd.concat([df, df_sa], axis=1)
+                # タイトルの感情分析結果の追加
+                sa = SentimentAnalyzer()
+                df_sa = sa.classify(df["name"].tolist())
+                df = pd.concat([df, df_sa], axis=1)
 
-    # データの保存
-    upload_file_to_s3(
-        df=df,
-        file_key=f"{dict_config['market']}/{dict_config['year']}/{dict_config['genre']}.csv",
-        bucket_name=BUCKET_NAME,
-    )
-    print("done")
+                # データの保存
+                upload_file_to_s3(
+                    df=df,
+                    file_key=f"{market}/{year}/{genre}.csv",
+                    bucket_name=BUCKET_NAME,
+                )
 
 
 if __name__ == "__main__":
--- util/load_config.py
+++ util/load_config.py
@@ -6,6 +6,7 @@ from yaml import safe_load
 def load_config(path: Path) -> dict:
     with open(path, encoding="utf-8") as f:
         config = safe_load(f)
-    config["year"] = int(config["year"])
+    config["start_year"] = int(config["start_year"])
+    config["end_year"] = int(config["end_year"])
     config["num_to_search"] = int(config["num_to_search"])
     return config
